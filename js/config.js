693250d71ab8b04cf517b711b5fa6045a781bf0b
itahmedosama-dotcom <it.ahmedosama@gmail.com>
2026-07-13 12:50:05 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 // ⚠️ تحذير: هذا الملف يظهر بالكامل لأي زائر للموقع (لأنه HTML/JS ثابت).
 // أي حد يقدر يشوف الرابط والمفتاح من كود الصفحة ويستخدمهم مباشرة.
 // ============================================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbwWoQQ0nEd84xIE2er-hGSnPMxHYxE90LOWpMoE5IVix52yzd7azjwmjfqqnVhYc47i/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbz-4JqJpcVIrSvCGP65Xd3odrz6ZWi5yui9aKeZN5b41vhzWJN4QP_NFTpUJLUQOUTV_Q/exec';
 const API_KEY = 'Nsh2026SecretKeyXyz';
